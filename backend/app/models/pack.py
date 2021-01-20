05a38baa2b517f19c1558aa7895acb5d511a4ddb
hirotasoshu <maksim.zaiakin@urfu.me>
2021-01-20 11:57:49 +0000

    Change type hints in models/pack.py
    Replace int with ProductionBatchNumber at batch_number fields

--- backend/app/models/pack.py
+++ backend/app/models/pack.py
@@ -3,11 +3,13 @@ from typing import Optional
 from odmantic import Model
 from pydantic import BaseModel
 
+from .production_batch import ProductionBatchNumber
+
 
 class Pack(Model):
     qr: str
     barcode: str
-    batch_number: Optional[int]
+    batch_number: Optional[ProductionBatchNumber]
     in_queue: bool = True
     created_at: Optional[str]
 
