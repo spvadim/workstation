92615dc01b2dbb4c56ead9be66fa41a07c3938ba
hirotasoshu <maksim.zaiakin@urfu.me>
2021-01-21 06:15:43 +0000

    Add comments and to_process feature to pack model
    Also update patch scheme

--- backend/app/models/pack.py
+++ backend/app/models/pack.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 
 from odmantic import Model
 from pydantic import BaseModel
@@ -10,6 +10,8 @@ class Pack(Model):
     qr: str
     barcode: str
     batch_number: Optional[ProductionBatchNumber]
+    comments: List[str] = []
+    to_process: bool = False
     in_queue: bool = True
     created_at: Optional[str]
 
@@ -22,8 +24,10 @@ class PackOutput(Model):
 class PackPatchSchema(BaseModel):
     qr: Optional[str]
     barcode: Optional[str]
+    comments: Optional[List[str]]
+    to_process: Optional[bool]
 
 
-class PackCameraInput(Model):
+class PackCameraInput(BaseModel):
     qr: Optional[str]
     barcode: Optional[str]
