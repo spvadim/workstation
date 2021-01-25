861debd8870e67cff3694132d1274d2f70d8ff55
root <root@db.devmhelper.ru>
2021-01-25 17:56:04 +0500

    update update_system.sh

--- update_system.sh
+++ update_system.sh
@@ -2,11 +2,11 @@
 branch=$(git rev-parse --abbrev-ref HEAD)
 echo "Here all the branches"
 git branch
-echo "Current branch is '$branch'. To change branch run 'git checkout <branch_name>'"
+echo "Current branch is '$branch'. To change branch run 'git checkout <branch_name>' or 'git checkuot -b <branch_name>' if branch is not in list"
 echo "Stopping everything"
 sudo docker-compose down
-read -p "Than switch to '$branch'. Press Enter to continue"
-git checkout $branch
+# read -p "Than switch to '$branch'. Press Enter to continue"
+# git checkout $branch
 read -p "Than pull changes from '$branch'. Press Enter to continue"
 git pull origin $branch
 read -p "Than rebuild docker containers and run them. Press Enter to continue"
