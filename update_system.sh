dc6daf39a57359565ff8e2b26fd1eb244460c54c
root <root@db.devmhelper.ru>
2021-01-25 16:56:26 +0500

    make updater universal

--- update_system.sh
+++ update_system.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-branch="main"
+branch=$(git rev-parse --abbrev-ref HEAD)
+echo "Here all the branches"
+git branch
+echo "Current branch is '$branch'. To change brunch run 'git checkout <branch_name>'"
 echo "Stopping everything"
 sudo docker-compose down
 read -p "Than switch to '$branch'. Press Enter to continue"
