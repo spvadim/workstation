db2d81a9d518f876ce0f20b267e44a74328f5e0e
hirotasoshu <maksim.zaiakin@urfu.me>
2021-01-26 07:58:25 +0000

    Update update_system.sh
    change command to delete dangling docker objects and update command to rebuild
    containers and run them

--- update_system.sh
+++ update_system.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "Delete all not needed docker images"
-docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+echo "Stopping everything"
+sudo docker-compose down
+echo "Delete all not needed docker objects"
+sudo docker system prune --force
 branch=$(git rev-parse --abbrev-ref HEAD)
 echo "Here all the branches"
 git branch
 echo "Current branch is '$branch'. To change branch run 'git checkout <branch_name>' or 'git checkout -b <branch_name>' if branch is not in list above"
-echo "Stopping everything"
-sudo docker-compose down
 # read -p "Than switch to '$branch'. Press Enter to continue"
 # git checkout $branch
 read -p "Than pull changes from '$branch'. Press Enter to continue"
@@ -17,4 +17,4 @@ git pull origin $branch
 #     echo "try to delete"
 # fi
 read -p "Than rebuild docker containers and run them. Press Enter to continue"
-sudo docker-compose up --build -d
+sudo COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose up --build -d
