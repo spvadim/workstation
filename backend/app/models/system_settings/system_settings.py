15e31539c28d6d4fc9e8b5af87161b962d5cea8e
hirotasoshu <maksim.zaiakin@urfu.me>
2021-01-29 13:04:04 +0000

    Fix import in system_settings

--- backend/app/models/system_settings/system_settings.py
+++ backend/app/models/system_settings/system_settings.py
@@ -2,11 +2,11 @@ from typing import Optional
 from odmantic import Model
 from pydantic import BaseModel
 
-from erd_settings import ERDSettings
-from general_settings import GeneralSettings
-from location_settings import LocationSettings
-from pintset_settings import PintsetSettings
-from telegram_settings import TGSettings
+from .erd_settings import ERDSettings
+from .general_settings import GeneralSettings
+from .location_settings import LocationSettings
+from .pintset_settings import PintsetSettings
+from .telegram_settings import TGSettings
 
 
 class SystemSettings(Model):
