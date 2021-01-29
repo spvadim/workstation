692d796aaa230eed75d4903526dc608f3e4700c4
hirotasoshu <maksim.zaiakin@urfu.me>
2021-01-29 10:47:22 +0000

    Add new SystemSettings model
    Also add patch schema for this model

---
+++ backend/app/models/system_settings/system_settings.py
@@ -0,0 +1,25 @@
+from typing import Optional
+from odmantic import Model
+from pydantic import BaseModel
+
+from erd_settings import ERDSettings
+from general_settings import GeneralSettings
+from location_settings import LocationSettings
+from pintset_settings import PintsetSettings
+from telegram_settings import TGSettings
+
+
+class SystemSettings(Model):
+    general_settings: GeneralSettings
+    location_settings: LocationSettings
+    erd_settings: ERDSettings
+    pintset_settings: PintsetSettings
+    telegram_settings: TGSettings
+
+
+class SystemSettingsPatchScheme(BaseModel):
+    general_settings: Optional[GeneralSettings]
+    location_settings: Optional[LocationSettings]
+    erd_settings: Optional[ERDSettings]
+    pintset_settings: Optional[PintsetSettings]
+    telegram_settings: Optional[TGSettings]
