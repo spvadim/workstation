1cebfa0536613cecaf43b938ec2fb5e335ac4c5f
hirotasoshu <maksim.zaiakin@urfu.me>
2021-02-11 20:15:54 +0000

    :zap: Change pack created_at and added_qr_at field to datetime
    :art: Add custom json_encoders for pretty datetime deserialization

--- backend/app/models/pack.py
+++ backend/app/models/pack.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from typing import List, Optional
 
 from odmantic import Model
+from odmantic.bson import BSON_TYPES_ENCODERS, ObjectId
 from pydantic import BaseModel
 
 from .production_batch import ProductionBatchNumber
@@ -13,14 +15,21 @@ class Pack(Model):
     comments: List[str] = []
     to_process: bool = False
     in_queue: bool = True
-    created_at: Optional[str]
+    created_at: Optional[datetime]
 
 
-class PackOutput(Model):
+class PackOutput(BaseModel):
+    id: ObjectId
     qr: str
-    created_at: str
+    created_at: datetime
     to_process: bool
     comments: List[str]
+
+    class Config:
+        json_encoders = {
+            **BSON_TYPES_ENCODERS, datetime:
+            lambda v: v.strftime("%d.%m.%Y %H:%M")
+        }
 
 
 class PackPatchSchema(BaseModel):
