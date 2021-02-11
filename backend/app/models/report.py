11ac52edfaba8e64cfbe6ed587042ed45516f382
hirotasoshu <maksim.zaiakin@urfu.me>
2021-02-11 20:25:25 +0000

    :zap: Change string datetime to naive datetime
    :art: Add custom json_encoders to keep nice date format

--- backend/app/models/report.py
+++ backend/app/models/report.py
@@ -1,25 +1,32 @@
 from typing import List, Optional
 from .production_batch import ProductionBatchNumber
+from datetime import datetime
 from pydantic import BaseModel
 
 
 class PackReportItem(BaseModel):
-    created_at: Optional[str]
+    created_at: Optional[datetime]
     batch_number: Optional[ProductionBatchNumber]
     qr: str
     barcode: str
 
+    class Config:
+        json_encoders = {datetime: lambda v: v.strftime("%d.%m.%Y %H:%M")}
+
 
 class MPackReportItem(BaseModel):
-    created_at: Optional[str]
+    created_at: Optional[datetime]
     batch_number: Optional[ProductionBatchNumber]
     qr: Optional[str]
     barcode: Optional[str]
     packs: List[PackReportItem] = []
 
+    class Config:
+        json_encoders = {datetime: lambda v: v.strftime("%d.%m.%Y %H:%M")}
+
 
 class CubeReportItem(BaseModel):
-    created_at: str
+    created_at: datetime
     batch_number: ProductionBatchNumber
     qr: Optional[str]
     barcode: Optional[str]
@@ -27,10 +34,16 @@ class CubeReportItem(BaseModel):
     packs_in_multipacks: int
     multipacks: List[MPackReportItem] = []
 
+    class Config:
+        json_encoders = {datetime: lambda v: v.strftime("%d.%m.%Y %H:%M")}
+
 
 class ReportRequest(BaseModel):
-    report_begin: Optional[str]
-    report_end: Optional[str]
+    report_begin: Optional[datetime]
+    report_end: Optional[datetime]
+
+    class Config:
+        json_encoders = {datetime: lambda v: v.strftime("%d.%m.%Y %H:%M")}
 
 
 class ReportResponse(ReportRequest):
