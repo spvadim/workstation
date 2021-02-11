276fef5f95c4967e8e13d5f563d412ccf1898d1c
hirotasoshu <maksim.zaiakin@urfu.me>
2021-02-11 20:23:35 +0000

    :zap: Create created_at index for packs, multipacks, cubes

--- backend/app/db/engine.py
+++ backend/app/db/engine.py
@@ -19,7 +19,10 @@ async def models_startup():
     await engine.get_collection(Pack).create_index('qr')
     await engine.get_collection(Pack).create_index(
         'in_queue', partialFilterExpression={'in_queue': True})
+    await engine.get_collection(Pack).create_index('created_at')
     await engine.get_collection(Multipack).create_index('status')
     await engine.get_collection(Multipack).create_index('qr')
+    await engine.get_collection(Multipack).create_index('created_at')
     await engine.get_collection(Cube).create_index('batch_number')
     await engine.get_collection(Cube).create_index('qr')
+    await engine.get_collection(Cube).create_index('created_at')
