732e2ab76c08bbffd92e5df451cda5785fa1919d
hirotasoshu <maksim.zaiakin@urfu.me>
2021-03-24 12:16:08 +0000

    :sparkles: Add get_last_pintset_amount, get_100_last_pintset_records

--- backend/app/db/db_utils.py
+++ backend/app/db/db_utils.py
@@ -6,6 +6,7 @@ from app.models.multipack import Multipack, Status
 from app.models.pack import Pack, PackInReport
 from app.models.pack import Status as PackStatus
 from app.models.packing_table import PackingTableRecord
+from app.models.pintset_record import PintsetRecord
 from app.models.production_batch import ProductionBatch, ProductionBatchNumber
 from app.models.report import (AnotherCubeReportItem, CubeReportItem,
                                CubeReportItemExtended, ExtendedReportResponse,
@@ -206,6 +207,15 @@ async def get_last_packing_table_amount() -> int:
     return multipacks_amount
 
 
+async def get_last_pintset_amount() -> int:
+    last_record = await engine.find_one(PintsetRecord,
+                                        sort=query.desc(PintsetRecord.id))
+    if not last_record:
+        return 0
+    packs_amount = last_record.packs_amount
+    return packs_amount
+
+
 async def get_batch_by_number_or_return_last(
         batch_number: Optional[ProductionBatchNumber]) -> ProductionBatch:
     if batch_number:
@@ -229,7 +239,8 @@ async def form_cube_from_n_multipacks(n: int) -> Cube:
     multipacks = await get_multipacks_on_packing_table()
     multipacks_for_cube = multipacks[:n]
     if len(multipacks_for_cube) < n:
-        raise HTTPException(400, detail='Недостаточно паллет для формирования куба')
+        raise HTTPException(400,
+                            detail='Недостаточно паллет для формирования куба')
     multipack_ids_with_pack_ids = {}
 
     for i in range(len(multipacks_for_cube)):
@@ -302,6 +313,13 @@ async def generate_multipack(batch_number, packs_in_multipacks,
 async def get_100_last_packing_records() -> List[PackingTableRecord]:
     records = await engine.find(PackingTableRecord,
                                 sort=query.desc(PackingTableRecord.id),
+                                limit=100)
+    return records
+
+
+async def get_100_last_pintset_records() -> List[PintsetRecord]:
+    records = await engine.find(PintsetRecord,
+                                sort=query.desc(PintsetRecord.id),
                                 limit=100)
     return records
 
