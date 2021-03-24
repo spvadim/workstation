73a56d854d76a9dc34746a730356eb412b987325
hirotasoshu <maksim.zaiakin@urfu.me>
2021-03-24 21:44:13 +0000

    :sparkles: Add functions to raise and finish damper and ejector
    Also split snmp_set and snmp_get to first_erd_snmp_set/get and second_erd_snmp_set/get

--- backend/app/utils/erd.py
+++ backend/app/utils/erd.py
@@ -7,7 +7,10 @@ on = Integer(1)
 off = Integer(0)
 
 
-async def set_cmd(key, value, engine=SnmpEngine(), context=ContextData()):
+async def first_erd_set_cmd(key,
+                            value,
+                            engine=SnmpEngine(),
+                            context=ContextData()):
     current_settings = await get_system_settings()
     erd_settings = current_settings.erd_settings
     community_string = erd_settings.erd_community_string.value
@@ -19,14 +22,14 @@ async def set_cmd(key, value, engine=SnmpEngine(), context=ContextData()):
 
 
 # изменение состояния
-async def snmp_set(key, value):
-    errorIndication, errorStatus, errorIndex, varBinds = await set_cmd(
+async def first_erd_snmp_set(key, value):
+    errorIndication, errorStatus, errorIndex, varBinds = await first_erd_set_cmd(
         key, value)
     for name, val in varBinds:
         return (val.prettyPrint())
 
 
-async def get_cmd(key, engine=SnmpEngine(), context=ContextData()):
+async def first_erd_get_cmd(key, engine=SnmpEngine(), context=ContextData()):
     current_settings = await get_system_settings()
     erd_settings = current_settings.erd_settings
     community_string = erd_settings.erd_community_string.value
@@ -38,8 +41,50 @@ async def get_cmd(key, engine=SnmpEngine(), context=ContextData()):
 
 
 # получение состояния
-async def snmp_get(key):
-    errorIndication, errorStatus, errorIndex, varBinds = await get_cmd(key)
+async def first_erd_snmp_get(key):
+    errorIndication, errorStatus, errorIndex, varBinds = await first_erd_get_cmd(
+        key)
+    for name, val in varBinds:
+        return (val.prettyPrint())
+
+
+async def second_erd_set_cmd(key,
+                             value,
+                             engine=SnmpEngine(),
+                             context=ContextData()):
+    current_settings = await get_system_settings()
+    erd_settings = current_settings.second_erd_settings
+    community_string = erd_settings.erd_community_string.value
+    port_snmp = erd_settings.erd_snmp_port.value
+    ip_address_host = erd_settings.erd_ip.value
+    return await setCmd(engine, CommunityData(community_string),
+                        UdpTransportTarget((ip_address_host, port_snmp)),
+                        context, ObjectType(ObjectIdentity(key), value))
+
+
+# изменение состояния
+async def second_erd_snmp_set(key, value):
+    errorIndication, errorStatus, errorIndex, varBinds = await second_erd_set_cmd(
+        key, value)
+    for name, val in varBinds:
+        return (val.prettyPrint())
+
+
+async def second_erd_get_cmd(key, engine=SnmpEngine(), context=ContextData()):
+    current_settings = await get_system_settings()
+    erd_settings = current_settings.second_erd_settings
+    community_string = erd_settings.erd_community_string.value
+    port_snmp = erd_settings.erd_snmp_port.value
+    ip_address_host = erd_settings.erd_ip.value
+    return await getCmd(engine, CommunityData(community_string),
+                        UdpTransportTarget((ip_address_host, port_snmp)),
+                        context, ObjectType(ObjectIdentity(key)))
+
+
+# получение состояния
+async def second_erd_snmp_get(key):
+    errorIndication, errorStatus, errorIndex, varBinds = await second_erd_get_cmd(
+        key)
     for name, val in varBinds:
         return (val.prettyPrint())
 
@@ -49,7 +94,7 @@ async def snmp_set_yellow_on():
 
     current_settings = await get_system_settings()
     yellow_oid = current_settings.erd_settings.erd_yellow_oid.value
-    await snmp_set(yellow_oid, on)
+    await first_erd_snmp_set(yellow_oid, on)
 
 
 async def snmp_set_yellow_off():
@@ -57,7 +102,7 @@ async def snmp_set_yellow_off():
 
     current_settings = await get_system_settings()
     yellow_oid = current_settings.erd_settings.erd_yellow_oid.value
-    await snmp_set(yellow_oid, off)
+    await first_erd_snmp_set(yellow_oid, off)
 
 
 async def snmp_set_red_on():
@@ -65,7 +110,7 @@ async def snmp_set_red_on():
 
     current_settings = await get_system_settings()
     red_oid = current_settings.erd_settings.erd_red_oid.value
-    await snmp_set(red_oid, on)
+    await first_erd_snmp_set(red_oid, on)
 
 
 async def snmp_set_red_off():
@@ -73,7 +118,7 @@ async def snmp_set_red_off():
 
     current_settings = await get_system_settings()
     red_oid = current_settings.erd_settings.erd_red_oid.value
-    await snmp_set(red_oid, off)
+    await first_erd_snmp_set(red_oid, off)
 
 
 async def snmp_set_green_on():
@@ -81,7 +126,7 @@ async def snmp_set_green_on():
 
     current_settings = await get_system_settings()
     green_oid = current_settings.erd_settings.erd_green_oid.value
-    await snmp_set(green_oid, on)
+    await first_erd_snmp_set(green_oid, on)
 
 
 async def snmp_set_green_off():
@@ -89,7 +134,7 @@ async def snmp_set_green_off():
 
     current_settings = await get_system_settings()
     green_oid = current_settings.erd_settings.erd_green_oid.value
-    await snmp_set(green_oid, off)
+    await first_erd_snmp_set(green_oid, off)
 
 
 async def snmp_set_buzzer_on():
@@ -97,7 +142,7 @@ async def snmp_set_buzzer_on():
 
     current_settings = await get_system_settings()
     buzzer_oid = current_settings.erd_settings.erd_buzzer_oid.value
-    await snmp_set(buzzer_oid, on)
+    await first_erd_snmp_set(buzzer_oid, on)
 
 
 async def snmp_set_buzzer_off():
@@ -105,4 +150,40 @@ async def snmp_set_buzzer_off():
 
     current_settings = await get_system_settings()
     buzzer_oid = current_settings.erd_settings.erd_buzzer_oid.value
-    await snmp_set(buzzer_oid, off)
+    await first_erd_snmp_set(buzzer_oid, off)
+
+
+async def snmp_raise_damper():
+    erd_logger.info('Подъем шторки')
+
+    current_settings = await get_system_settings()
+    first_oid = current_settings.second_erd_settings.erd_first_oid.value
+    if first_oid:
+        await second_erd_snmp_set(first_oid, on)
+
+
+async def snmp_finish_damper():
+    erd_logger.info('Опускание шторки')
+
+    current_settings = await get_system_settings()
+    first_oid = current_settings.second_erd_settings.erd_first_oid.value
+    if first_oid:
+        await second_erd_snmp_set(first_oid, off)
+
+
+async def snmp_raise_ejector():
+    erd_logger.info('Подъем сбрасывателя')
+
+    current_settings = await get_system_settings()
+    second_oid = current_settings.second_erd_settings.erd_second_oid.value
+    if second_oid:
+        await second_erd_snmp_set(second_oid, on)
+
+
+async def snmp_finish_ejector():
+    erd_logger.info('Опускание сбрасывателя')
+
+    current_settings = await get_system_settings()
+    second_oid = current_settings.second_erd_settings.erd_second_oid.value
+    if second_oid:
+        await second_erd_snmp_set(second_oid, off)
