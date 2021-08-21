fe9867f488e509d2d8bd4066c17741150c146fdd
Oleg "OSA413" Sokolov <OSA413@users.noreply.github.com>
2021-08-21 09:48:28 +0500

    Replaced node-sass with sass

--- frontend/src/pages/Admin/Settings/future-SettingsBlock.tsx
+++ frontend/src/pages/Admin/Settings/future-SettingsBlock.tsx
@@ -1,5 +1,5 @@
 import { Setting, SettingsGroup, Settings } from "./future-SettingsInterface";
-//import "./SettingsBlock.scss";
+import "./SettingsBlock.scss";
 import ToolTip from "src/components/ToolTip";
 
 const SettingsBlock = (settings: Settings, editSettings: Settings
